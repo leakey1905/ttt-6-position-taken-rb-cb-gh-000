9879d87033314946e8c6d747e8abef0d63eb9000
Ji Li <liji15@mails.tsinghua.edu.cn>
2018-11-20 07:09:51 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,6 +1,9 @@
 # code your #position_taken? method here!
 def position_taken?(borad, index)
-  if board[index] != " "
+  if board[index] == " " or board[index] == ""
+    return true
+  else
     return false
-    
+  end
+
 end
